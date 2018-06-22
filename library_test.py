85ef93fefddb9f05442abdfcb8e8ed88dd9b7efa
ashcherbakov <alexander.sherbakov@dsr-company.com>
2018-06-22 11:54:43 +0300

    support parsing of dates in iso6801 and text formats

--- library_test.py
+++ library_test.py
@@ -1,4 +1,5 @@
 import unittest
+
 import library
 
 NUM_CORPUS = '''
@@ -10,8 +11,8 @@ US until 15-20 years later.) It is celebrated by 77.9% of the population--
 trending toward 80.                                                                
 '''
 
-class TestCase(unittest.TestCase):
 
+class TestCase(unittest.TestCase):
     # Helper function
     def assert_extract(self, text, extractors, *expected):
         actual = [x[1].group(0) for x in library.scan(text, extractors)]
@@ -26,9 +27,93 @@ class TestCase(unittest.TestCase):
     def test_integers(self):
         self.assert_extract(NUM_CORPUS, library.integers, '1845', '15', '20', '80')
 
+    def test_integers_with_grouping(self):
+        self.assert_extract('aaa 123,456,789 bbb', library.integers, '123,456,789')
+        self.assert_extract('123,456,789', library.integers, '123,456,789')
+        self.assert_extract('aaa 123,456,789 456,678 bbb', library.integers, '123,456,789', '456,678')
+        self.assert_extract('123,456,789 456,678', library.integers, '123,456,789', '456,678')
+        self.assert_extract('aaa 123,456,789 456,678 456 bbb', library.integers, '123,456,789', '456,678', '456')
+        self.assert_extract('123,456,789 456', library.integers, '123,456,789', '456')
+
     # Third unit test; prove that if we look for integers where there are none, we get no results.
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
+
+    # prove that if we look for dates, we find them.
+    def test_dates_iso8601(self):
+        self.assert_extract('I was born on 2015-07-25.', library.dates_iso8601, '2015-07-25')
+
+    def test_dates_iso8601_timestamp_millisec(self):
+        self.assert_extract('I was born on 2015-07-25 18:22:19.123.', library.dates_iso8601, '2015-07-25 18:22:19.123')
+
+    def test_dates_iso8601_timestamp_sec(self):
+        self.assert_extract('I was born on 2015-07-25 18:22:19.', library.dates_iso8601, '2015-07-25 18:22:19')
+
+    def test_dates_iso8601_timestamp_min(self):
+        self.assert_extract('I was born on 2015-07-25 18:22.', library.dates_iso8601, '2015-07-25 18:22')
+
+    def test_dates_iso8601_timestamp_delimiter(self):
+        self.assert_extract('I was born on 2015-07-25T18:22:19.123.', library.dates_iso8601, '2015-07-25T18:22:19.123')
+        self.assert_extract('I was born on 2015-07-25T18:22:19.', library.dates_iso8601, '2015-07-25T18:22:19')
+        self.assert_extract('I was born on 2015-07-25T18:22.', library.dates_iso8601, '2015-07-25T18:22')
+
+    def test_dates_iso8601_timezone_3letter(self):
+        self.assert_extract('I was born on 2015-07-25 18:22:19.123MDT.', library.dates_iso8601,
+                            '2015-07-25 18:22:19.123MDT')
+        self.assert_extract('I was born on 2015-07-25T18:22:19.123MDT.', library.dates_iso8601,
+                            '2015-07-25T18:22:19.123MDT')
+        self.assert_extract('I was born on 2015-07-25 18:22:19MDT.', library.dates_iso8601, '2015-07-25 18:22:19MDT')
+        self.assert_extract('I was born on 2015-07-25 18:22MDT.', library.dates_iso8601, '2015-07-25 18:22MDT')
+
+    def test_dates_iso8601_timezone_1letter(self):
+        self.assert_extract('I was born on 2015-07-25 18:22:19.123Z.', library.dates_iso8601,
+                            '2015-07-25 18:22:19.123Z')
+        self.assert_extract('I was born on 2015-07-25T18:22:19.123Z.', library.dates_iso8601,
+                            '2015-07-25T18:22:19.123Z')
+        self.assert_extract('I was born on 2015-07-25 18:22:19Z.', library.dates_iso8601, '2015-07-25 18:22:19Z')
+        self.assert_extract('I was born on 2015-07-25 18:22Z.', library.dates_iso8601, '2015-07-25 18:22Z')
+
+    def test_dates_iso8601_timezone_offset(self):
+        self.assert_extract('I was born on 2015-07-25 18:22:19.123-0800.',
+                            library.dates_iso8601, '2015-07-25 18:22:19.123-0800')
+        self.assert_extract('I was born on 2015-07-25T18:22:19.123-0800.', library.dates_iso8601,
+                            '2015-07-25T18:22:19.123-0800')
+        self.assert_extract('I was born on 2015-07-25 18:22:19-0800.', library.dates_iso8601,
+                            '2015-07-25 18:22:19-0800')
+        self.assert_extract('I was born on 2015-07-25 18:22-0800.', library.dates_iso8601,
+                            '2015-07-25 18:22-0800')
+
+    # prove that if we look for dates, then the month must be in the interval [1,12]
+    def test_dates_iso8601_incorrect_month(self):
+        self.assert_extract('I was born on 2015-13-25.', library.dates_iso8601)
+        self.assert_extract('I was born on 2015-00-25.', library.dates_iso8601)
+        self.assert_extract('I was born on 2015-99-25.', library.dates_iso8601)
+
+    # prove that if we look for dates, then the day must be in the interval [1,31]
+    def test_dates_iso8601_incorrect_day(self):
+        self.assert_extract('I was born on 2015-07-00.', library.dates_iso8601)
+        self.assert_extract('I was born on 2015-07-32.', library.dates_iso8601)
+        self.assert_extract('I was born on 2015-07-99.', library.dates_iso8601)
+
+    def test_dates_fmt2(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_fmt2, '25 Jan 2017')
+
+    def test_dates_fmt2_with_comma(self):
+        self.assert_extract('I was born on 25 Jan, 2017.', library.dates_fmt2, '25 Jan, 2017')
+
+    def test_dates_fmt2_multiple(self):
+        self.assert_extract('I was born on 25 Jan 2017 and you were born on 01 Dec 1901.', library.dates_fmt2,
+                            '25 Jan 2017', '01 Dec 1901')
+
+    def test_dates_iso8601_multiple(self):
+        self.assert_extract('I was born on 2015-07-27 and you were born on 1900-07-01.', library.dates_iso8601,
+                            '2015-07-27', '1900-07-01')
+
+    def test_dates_fmt2_no_year(self):
+        self.assert_extract('I was born on 25 Jan and you were born on 01 Dec.', library.dates_fmt2)
+
+    def test_dates_iso8601_no_year(self):
+        self.assert_extract('I was born on 01-25 and you were born on 01-01.', library.dates_fmt2)
 
 
 if __name__ == '__main__':
